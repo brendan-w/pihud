f65f07d0320626cae76900c66565ecd622eed6ad
Andrew AJ Mandula <ajman@csh.rit.edu>
2014-11-23 17:04:57 -0500

    no longer saving out a svg, using a bytearray instead

--- pyHudTest.py
+++ pyHudTest.py
@@ -68,9 +68,10 @@ class Example(QtGui.QWidget):
 
         bar_chart = pygal.Bar(width= 300 ,height = 200)                                
         bar_chart.add('Fibonacci', [0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55]) 
-        bar_chart.render_to_file('bar_chart.svg') 
+        chart = QtCore.QByteArray (bar_chart.render())
+        # bar_chart.render_to_file('bar_chart.svg') 
 
-        svg1 =  SVGWidget(self,'bar_chart.svg')
+        svg1 =  SVGWidget(self,chart)
         self.draggables.append(svg1);
 
         self.setWindowTitle('PyHud')
