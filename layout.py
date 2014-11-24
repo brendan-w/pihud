21f842ee97dc11cf37f9479c643027e27d581619
Brendan Whitfield <brendanw@windworksdesign.com>
2014-11-24 13:13:15 -0500

    removed button

--- layout.py
+++ layout.py
@@ -2,30 +2,8 @@ from PyQt4 import QtCore, QtGui, QtSvg
 import pygal
 
 
-class Button(QtGui.QPushButton):
-  
-    def __init__(self, title, parent):
-        super(Button, self).__init__(title, parent)
-
-    def mouseMoveEvent(self, e):
-
-        if e.buttons() == QtCore.Qt.RightButton:
-            drag = QtGui.QDrag(self)
-            drag.setMimeData(QtCore.QMimeData())
-            drag.setHotSpot(e.pos() - self.rect().topLeft())
-
-            dropAction = drag.start(QtCore.Qt.MoveAction)
-
-    def mousePressEvent(self, e):
-      
-        super(Button, self).mousePressEvent(e)
-        
-        if e.button() == QtCore.Qt.LeftButton:
-            print 'press'
-
-
 class SVGWidget(QtSvg.QSvgWidget):
-    
+
     def __init__(self, parent, byteArray):
         super(SVGWidget,self).__init__(parent)
         super(SVGWidget,self).load(byteArray)
@@ -40,56 +18,37 @@ class SVGWidget(QtSvg.QSvgWidget):
             dropAction = drag.start(QtCore.Qt.MoveAction)
 
     def mousePressEvent(self, e):
-      
+
         super(SVGWidget, self).mousePressEvent(e)
 
 
 class Example(QtGui.QWidget):
-  
+
     def __init__(self, parent=None):
         super(Example, self).__init__(parent)
         self.draggables = []
 
         self.initUI()
-        
+
     def initUI(self):
 
         self.setAcceptDrops(True)
 
-        button = Button('Button', self)
-        button.move(600, 65)
-        self.draggables.append(button);
-
-        bar_chart = pygal.Bar(width= 300 ,height = 200)                                
-        bar_chart.add('Fibonacci', [0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55]) 
+        bar_chart = pygal.Bar(width= 300 ,height = 200)
+        bar_chart.add('Fibonacci', [0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55])
         chart = QtCore.QByteArray(bar_chart.render())
 
         svg1 = SVGWidget(self, chart)
-        self.draggables.append(svg1);
-
-        # self.setWindowTitle('PyHud')
-        # self.setGeometry(300, 300, 280, 150)
-        # self.show()
+        self.draggables.append(svg1)
 
     def dragEnterEvent(self, e):
-      
+
         e.accept()
 
     def dropEvent(self, e):
 
-        position = e.pos()        
-        e.source().move(position)  
+        position = e.pos()
+        e.source().move(position)
 
         e.setDropAction(QtCore.Qt.MoveAction)
         e.accept()
-
-'''
-if __name__ == '__main__':
-
-    import sys
-
-    app = QtGui.QApplication(sys.argv)
-    ex = Example()
-
-    sys.exit(app.exec_())
-'''
