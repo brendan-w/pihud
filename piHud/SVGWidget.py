91138f531af51edc47a66305ca5bb7c18d82f7e2
Brendan Whitfield <brendanw@windworksdesign.com>
2014-12-03 01:04:24 -0500

    manual merge from demo branch, moved byteArray and CSS fix into superclass

--- piHud/SVGWidget.py
+++ piHud/SVGWidget.py
@@ -17,3 +17,15 @@ class SVGWidget(QtSvg.QSvgWidget):
 
     def mousePressEvent(self, e):
         super(SVGWidget, self).mousePressEvent(e)
+
+    def load(self, svg):
+        """ override the QT load function for ease of handling pygal SVGs """
+
+        # PyQt has a rendering issue with the following CSS
+        # delete it by replacing it with empty brackets
+        # https://github.com/brendanwhitfield/piHud/issues/2
+        svg = svg.replace(" .series{stroke-width:1.0;stroke-linejoin:round;stroke-linecap:round;stroke-dasharray:0,0}", " .series{}")
+
+        # wrap in PyQt byteArray
+        byteArray = QtCore.QByteArray(svg)
+        super(SVGWidget, self).load(byteArray)
--- piHud/widgets.py
+++ piHud/widgets.py
@@ -1,22 +1,29 @@
 from PyQt4 import QtCore, QtGui, QtSvg
 import obd
 import pygal
+from pygal.style import Style
 from SVGWidget import SVGWidget
 
 
 class Gauge(SVGWidget):
     def __init__(self, parent, command):
         super(Gauge, self).__init__(parent)
-        super(Gauge, self).setFixedWidth(500)
+        super(Gauge, self).setFixedWidth(350)
         super(Gauge, self).setFixedHeight(400)
 
         self.command = command
-
-    def get_command(self):
-        return self.command
+        self.style = Style(
+            background='black',
+            plot_background='transparent',
+            foreground='#53B9E8',
+            foreground_light='#53A0E8',
+            foreground_dark='transparent',
+            colors=('#53B9E8', '#53B9E8'))
 
     def render(self, response):
-        gauge_chart = pygal.Gauge(human_readable=True)
+        gauge_chart = pygal.Gauge(human_readable=True, width=200, height=250, style=self.style)
+        
+        gauge_chart.show_legend = False
         gauge_chart.title = self.command.name
         gauge_chart.range = [0, 8000]
 
@@ -25,5 +32,5 @@ class Gauge(SVGWidget):
             value = response.value
 
         gauge_chart.add(self.command.name, value)
-        chart = QtCore.QByteArray(gauge_chart.render())
-        super(Gauge, self).load(chart)
+
+        self.load(gauge_chart.render())
