b31e5d7f3292bc848be25fb839e6b5d653011a79
Brendan Whitfield <brendanw@windworksdesign.com>
2015-02-02 22:27:52 -0500

    added needle indicator

--- piHud/widgets/Gauge.py
+++ piHud/widgets/Gauge.py
@@ -1,23 +1,29 @@
 
-from PyQt4 import QtCore, QtGui
+from PyQt4.QtCore import *
+from PyQt4.QtGui import *
+
 from math import log10
 from util import map_value
 
 
-class Gauge(QtGui.QWidget):
+class Gauge(QWidget):
     def __init__(self, parent, config):
         super(Gauge, self).__init__(parent)
-        self.color = QtGui.QColor(config.color)
-        self.pen   = QtGui.QPen(self.color)
+        self.config = config
+
+        self.color = QColor(config.color)
+        self.pen   = QPen(self.color)
+        self.brush = QBrush(self.color)
         self.pen.setWidth(2)
 
         # choose a smart scale step
-        scale_len   = config.max - config.min
-        scale_order = round(log10(scale_len))
-        scale_step  = 10 ** (scale_order - 1)        
+        scale_len      = config.max - config.min
+        self.scale_len = scale_len
+        scale_order    = round(log10(scale_len))
+        scale_step     = 10 ** (scale_order - 1)        
         
         #                       [      Widget Units     ] [Angle]
-        angle_step  = map_value(scale_step, 0, scale_len, 0, 270)
+        angle_step  = map_value(scale_step, config.min, config.max, 0, 270)
         scale_ticks = int(scale_len // scale_step)
         end_tick    = bool(scale_len % scale_step)
 
@@ -29,15 +35,17 @@ class Gauge(QtGui.QWidget):
 
 
     def sizeHint(self):
-        return QtCore.QSize(300, 300)
+        return QSize(300, 300)
 
 
     def paintEvent(self, e):
-        painter = QtGui.QPainter()
+        painter = QPainter()
         painter.begin(self)
-        painter.setRenderHint(QtGui.QPainter.Antialiasing)
+        painter.setPen(self.pen)
+        painter.setRenderHint(QPainter.Antialiasing)
         
         self.draw_marks(painter)
+        self.draw_needle(painter, 1000)
 
         painter.end()
 
@@ -45,7 +53,6 @@ class Gauge(QtGui.QWidget):
     def draw_marks(self, painter):
         painter.save()
 
-        painter.setPen(self.pen)
         painter.translate(self.width() / 2, self.height() / 2)
         painter.rotate(90 + 45)
 
@@ -55,5 +62,29 @@ class Gauge(QtGui.QWidget):
         for a in self.scale:
             painter.drawLine(x_start, 0, x_end, 0)
             painter.rotate(a)
+
+        painter.restore()
+
+
+    def draw_needle(self, painter, value):
+        painter.save()
+
+        painter.translate(self.width() / 2, self.height() / 2)
+        angle = map_value(value, self.config.min, self.config.max, 0, 270)
+        angle -= 90 + 45
+        painter.rotate(angle)
+
+        painter.setBrush(self.brush)
+
+        painter.drawEllipse(QPoint(0,0), 5, 5)
+
+        painter.drawPolygon(
+            QPolygon([
+                QPoint(-5, 0),
+                QPoint(0,   -(self.width() / 20) * 8),
+                QPoint(5,  0),
+                QPoint(-5, 0)
+            ])
+        )
 
         painter.restore()
